0517d2db1c01077f36f334357bbd89b4015e6447
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-09-16 10:17:29 +0200

    Provide an example for the grammar

--- src/grammar.rs
+++ src/grammar.rs
@@ -8,6 +8,31 @@
 //!    | backward S
 //!    | ''
 //! ```
+//!
+//! # Examples
+//! We  show how to create a `Program` by hand
+//!
+//! ```rust
+//! extern crate nadezhda;
+//!
+//! use nadezhda::grammar::Program;
+//!
+//! fn main() {
+//!     let program: Program = Program::Forward(
+//!       Box::new(Program::Forward(
+//!         Box::new(Program::Stop)
+//!       ))
+//!     );
+//!
+//!     let message = match program {
+//!       Program::Forward(_)  => "right",
+//!       Program::Backward(_) => "wrong",
+//!       Program::Stop        => "not even close",
+//!     };
+//!
+//!     assert_eq!(message, "right");
+//! }
+//! ```
 
 /// A `Program` is either
 ///
