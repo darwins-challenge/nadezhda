f8e188c5f966628bcde43897a6593702e7af2e48
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-09-16 07:08:30 +0200

    Describe project in documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 #![deny(missing_docs)]
+//! Nadezhda is a project to model a robot space cockroach.
+//!
+//! > The cockroach moves along a line. Each turn it either takes a step
+//! > forward, takes a step backwards, or stops.
+//!
+//! See [fly me to the moon](https://leanpub.com/flymetothemoon) for a extended
+//! description.
