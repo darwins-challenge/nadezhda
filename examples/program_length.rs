f5b5682bb414f326ab72c2068211b4de6a9182a7
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-09-16 16:40:18 +0200

    Demonstrate the average length of a program

--- examples/program_length.rs
+++ examples/program_length.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 extern crate nadezhda;
 
+use std::collections::HashMap;
 use nadezhda::grammar::Program;
 
 trait Length {
@@ -23,8 +24,27 @@ impl Length for Program {
 }
 
 fn main() {
-    let program: Program = rand::random();
+    let mut length_frequency = HashMap::new();
+    let number_of_loops = 10_000;
+    for _ in 0..number_of_loops {
+        let program: Program = rand::random();
+        let length = program.length();
 
-    println!("length of program is {}", program.length());
+        let count = match length_frequency.get(&length) {
+            Some(last_value) => last_value + 1,
+            None => 1
+        };
+        length_frequency.insert(length, count);
+    }
+
+    let mut lengths: Vec<&i32> = length_frequency.keys().collect();
+    lengths.sort();
+    let mut weighted_sum = 0;
+    for length in lengths {
+        let frequency = length_frequency.get(length).unwrap();
+        weighted_sum += length * frequency;
+        println!("{}: {}", length, frequency );
+    }
+    println!("averge {}", (weighted_sum as f32) / (number_of_loops as f32));
 }
 
