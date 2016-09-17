6ced0764efc69804371f837b1a32614f032287fc
Daan van Berkel <daan@fifth-postulate.nl>
2016-09-17 20:04:54 +0000

    rebuild pages at 248b0fe

---
+++ nadezhda/fitness/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Config","Configuration for a default implementation for a ScoreCard"]],"trait":[["ScoreCard","A scorecard can keep score a program on an environment"]],"type":[["Score","A score is just a number"]]});
