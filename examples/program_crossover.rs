f7c02c5465cdcaccce1a5bb9505a722180f8f517
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-09-20 17:09:46 +0200

    Provide an example of a crossover

---
+++ examples/program_crossover.rs
@@ -0,0 +1,68 @@
+extern crate rand;
+#[macro_use]
+extern crate nadezhda;
+
+use std::collections::HashSet;
+use nadezhda::grammar::Program;
+use nadezhda::darwin::crossover::crossover;
+
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+struct FingerPrint {
+    f: i32,
+    b: i32,
+}
+
+impl FingerPrint {
+    fn new() -> FingerPrint {
+    FingerPrint {
+        f: 0,
+        b: 0,
+    }
+}
+
+fn f(&self) -> FingerPrint {
+FingerPrint { f: self.f + 1, ..*self }
+    }
+
+    fn b(&self) -> FingerPrint {
+        FingerPrint { b: self.b + 1, ..*self }
+    }
+}
+
+trait FingerPrintable {
+    fn finger_print(&self) -> FingerPrint;
+}
+
+impl FingerPrintable for Program {
+    fn finger_print(&self) -> FingerPrint {
+        match *self {
+            Program::Forward(ref contained_program) => contained_program.finger_print().f(),
+            Program::Backward(ref contained_program) => contained_program.finger_print().b(),
+            Program::Stop => FingerPrint::new(),
+        }
+    }
+}
+
+fn main() {
+    let mut finger_prints: HashSet<FingerPrint> = HashSet::new();
+    let mut rng = rand::thread_rng();
+    let left: Program = forward!(forward!(forward!(stop!())));
+    let right: Program = backward!(backward!(backward!(stop!())));
+
+    let mut count = 0;
+    loop {
+        let crossover = crossover(&mut rng, &left, &right);
+        let finger_print = crossover.finger_print();
+        finger_prints.insert(finger_print);
+        count += 1;
+
+        if finger_prints.len() == 16 { break; }
+    }
+
+    let mut prints: Vec<&FingerPrint> = finger_prints.iter().collect();
+    prints.sort();
+    println!("{}", count);
+    for print in prints {
+        println!("{:?}", print);
+    }
+}
