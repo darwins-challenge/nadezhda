8fc6ace89d7705cf1a461315a9abb98e99fe4848
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-09-22 16:39:45 +0200

    Show survivor if there is one

--- examples/evolve.rs
+++ examples/evolve.rs
@@ -7,6 +7,18 @@ use nadezhda::population::Population;
 use nadezhda::evaluate::Evaluator;
 use nadezhda::darwin::evolve::succesion;
 
+fn survivors(population: Population, environment: Environment) -> Vec<Program> {
+    let Population(generation) = population;
+    let survivors: Vec<Program> = generation
+        .iter()
+        .map(|program| (program.clone(), program.evaluate_on(environment.clone())))
+        .filter(|&(_, ref env)| env.cockroach_location - env.food_location == 0)
+        .map(|(program, _)| program)
+        .collect();
+
+    survivors
+}
+
 fn main() {
     let mut rng = rand::thread_rng();
     let environment: Environment = Environment::new(5);
@@ -15,19 +27,18 @@ fn main() {
     let mut generation_count = 0;
     while generation_count < 100 {
         let next_population = succesion(&mut rng, environment.clone(), last_population.clone());
-        let Population(generation) = next_population.clone();
-        let survivors: Vec<Program> = generation
-            .iter()
-            .map(|program| (program.clone(), program.evaluate_on(environment.clone())))
-            .filter(|&(_, ref env)| env.cockroach_location - env.food_location == 0)
-            .map(|(program, _)| program)
-            .collect();
+
+        let survivors: Vec<Program> = survivors(next_population.clone(), environment.clone());
+        last_population = next_population;
 
         if survivors.len() > 0 { break; }
-
-        last_population = next_population;
         generation_count += 1;
     }
-    println!("{} {:?}", generation_count, last_population);
 
+    let survivors: Vec<Program> = survivors(last_population.clone(), environment.clone());
+    if survivors.len() > 0 {
+        println!("{} {} {:?}", generation_count, survivors.len(), survivors[0]);
+    } else {
+        println!("{} {:?}", generation_count, last_population);
+    }
 }
