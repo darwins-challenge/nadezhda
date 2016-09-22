8a8dc7a18a71e84cb1f8049a2de5b0c0ee328fa4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-09-22 15:24:34 +0200

    Let succession do crossover

--- examples/evolve.rs
+++ examples/evolve.rs
@@ -6,10 +6,13 @@ use nadezhda::population::Population;
 use nadezhda::darwin::evolve::succesion;
 
 fn main() {
+    let mut rng = rand::thread_rng();
     let environment: Environment = Environment::new(5);
     let last_population: Population = Population::new(5);
 
-    let next_population = succesion(environment, last_population);
+    println!("{:?}", last_population);
+
+    let next_population = succesion(&mut rng, environment, last_population);
 
     println!("{:?}", next_population);
 
--- src/darwin/evolve.rs
+++ src/darwin/evolve.rs
@@ -4,13 +4,16 @@
 //! harsh environment for limited resources. Here the environment is determined
 //! by the fitness function, the individuals are programs.
 
+use rand;
+use rand::distributions::{Range, IndependentSample};
 use super::super::grammar::Program;
 use super::super::population::Population;
 use super::super::environment::Environment;
 use super::super::fitness::{Config, ScoreCard, Score};
+use super::crossover::crossover;
 
 /// Return the next generation of the population
-pub fn succesion(environment: Environment, population: Population) -> Population {
+pub fn succesion(mut rng: &mut rand::Rng, environment: Environment, population: Population) -> Population {
     let config: Config = Config {
         program_length_weight: -1,
         food_distance_weight: -1,
@@ -21,10 +24,24 @@ pub fn succesion(environment: Environment, population: Population) -> Population
         .map(|program| (program.clone(), config.score(program.clone(), environment.clone())))
         .collect();
     scored_last_generation.sort_by(|a,b| b.1.cmp(&a.1));
-    let next_generation = scored_last_generation
-        .iter()
-        .map(|a| a.0.clone())
-        .collect();
+
+    let mut next_generation: Vec<Program> = vec!();
+    for index in 0..2 {
+        let program: Program = scored_last_generation[index].0.clone();
+        next_generation.push(program);
+    }
+    let index_range = Range::new(0, scored_last_generation.len());
+    while next_generation.len() < last_generation.len() {
+        let left_index = index_range.ind_sample(&mut rng);
+        let right_index = index_range.ind_sample(&mut rng);
+        let left = scored_last_generation[left_index].0.clone();
+        let right = scored_last_generation[right_index].0.clone();
+        let (left_child, right_child) = crossover(&mut rng, &left, &right);
+        next_generation.push(left_child);
+        if next_generation.len() < last_generation.len() {
+            next_generation.push(right_child);
+        }
+    }
 
     Population(next_generation)
 }
