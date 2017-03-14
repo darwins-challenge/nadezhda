7cac74c78dbb9409bfd212ed22f960711f822846
Daan van Berkel <daan@fifth-postulate.nl>
2017-03-14 20:44:40 +0000

    rebuild pages at f7dbe6e

--- implementors/rand/trait.Rand.js
+++ implementors/rand/trait.Rand.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
-implementors["rand"] = [];implementors["nadezhda"] = ["impl <a class='trait' href='rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='enum' href='nadezhda/grammar/enum.Program.html' title='nadezhda::grammar::Program'>Program</a>",];
+implementors["nadezhda"] = ["impl <a class='trait' href='rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='enum' href='nadezhda/grammar/enum.Program.html' title='nadezhda::grammar::Program'>Program</a>",];
+implementors["rand"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
